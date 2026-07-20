d9669b2a24d92b227e98ad90deb6d54a10f86742
kirisawa0 <younes.m.goldero2004@gmail.com>
2026-07-20 10:00:52 +0200

    MAJ4

---
+++ js/materiel.js
@@ -0,0 +1,56 @@
+import { insertarMaterial } from "./insertarMaterial.js";
+
+const boutonFrom = document.querySelector("#bouton-bodega");
+const boutonAnnuler = document.querySelector("#bouton-annuler-bodega");
+const boutonSauvegarder = document.querySelector("#bouton-sauv-bodega");
+const formulaire = document.querySelector("#form-bodega");
+
+boutonFrom.addEventListener("click", apparaitreform);
+
+boutonAnnuler.addEventListener("click", annuler);
+
+function apparaitreform(){
+    formulaire.hidden = false;
+    boutonFrom.hidden = true;
+
+    document.querySelector("#nom-bodega").focus();
+}
+
+
+function annuler(){
+    formulaire.reset();
+    formulaire.hidden = true;
+    boutonFrom.hidden = false;
+
+}
+
+
+
+formulaire.addEventListener("submit", ajoutMat);
+
+async function ajoutMat(event) {
+    event.preventDefault();
+
+    boutonSauvegarder.disabled = true;
+
+    try {
+        await insertarMaterial("material_bodega", {
+        nom: document.querySelector("#nom-bodega").value,
+        quantite: document.querySelector("#quantite-bodega").value,
+        lieu:
+            document.querySelector("#lieu-bodega").value.trim() || null,
+        etat: "disponible"
+        });
+
+        formulaire.reset();
+        formulaire.hidden = true;
+        boutonFrom.hidden = false;
+
+        alert("Materiel ajouté correctement");
+        } catch (error) {
+            alert(`Error: ${error.message}`);
+        } finally {
+            boutonSauvegarder.disabled = false;
+        }
+
+}
